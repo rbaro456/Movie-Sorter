fd001961e97f8666dad816556f039acc13e78337
rbaro456 <rbaro456@gmail.com>
2017-09-28 21:49:36 -0400

    Add files via upload
    You this to test your merge sort. Everything you need in main is there, do not change it. Just add your merge sort method. Must enter everything in command line properly for it to work.

---
+++ sorter.h
@@ -0,0 +1,21 @@
+#ifndef SORTER_H_INCLUDED
+#define SORTER_H_INCLUDED
+
+typedef struct node {
+
+
+    char** category;
+
+    struct node* next;
+
+}node;
+
+char** parseCSV(char* line);
+
+void addNode(char** splitString);
+
+int getSearchIndex(char* argv[], char** cat_name);
+
+void csvOutput(node* root);
+
+#endif // SORTER_H_INCLUDED
